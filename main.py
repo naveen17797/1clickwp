9464c6995d1d60d4b275e81f8815ef58e0c8e369
naveen <kmnaveen101@gmail.com>
2023-10-23 16:49:47 +0530

    move init db to the startup hook

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from typing import Annotated
 
+import docker
 from fastapi import FastAPI, Path
 from pydantic import BaseModel
 
 from utils import DB, WordPress
 
-db = DB()
-db.init_db()
+
 
 app = FastAPI()
 
@@ -53,3 +53,25 @@ async def delete( site_id: Annotated[str, Path(title="The ID of the site to dele
 @app.get("/")
 async def root():
     return []
+
+
+# Remove and prune containers on shutdown hook
+# Function to stop and remove containers with a specific prefix
+def stop_and_remove_containers(prefix: str):
+    client = docker.from_env()
+    containers = client.containers.list(all=True, filters={"name": f"{prefix}*"})
+    for container in containers:
+        container.stop()
+        container.remove()
+
+@app.on_event("shutdown")
+async def cleanup():
+    # Stop and remove containers with prefix "1clickwp"
+    stop_and_remove_containers("1clickwp")
+
+@app.on_event("startup")
+async def cleanup():
+    # Remove orphaned containers.
+    stop_and_remove_containers("1clickwp")
+    db = DB()
+    db.init_db()
