3adddf61a6657cdeb4efc9e0f931747d68412a2e
naveen <kmnaveen101@gmail.com>
2023-10-24 13:21:02 +0530

    add init files for ui

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'ui';
+  loading = true;
+  wpVersion = '';
+  multisite = false;
+  sites: any[] = [];
+
+
+
+  siteForm: FormGroup;
+
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.siteForm = this.fb.group({
+      version: ['', Validators.required],
+      multi_site: [false]
+    });
+  }
+
+  ngOnInit() {
+    this.checkImage();
+    this.listSites();
+  }
+
+  checkImage() {
+    this.http.get('/images/mysql:5.7').subscribe(
+      () => {
+        this.loading = false;
+      },
+      () => {
+        this.pullImage();
+      }
+    );
+  }
+
+  pullImage() {
+    this.http.post('/images/mysql:5.7/pulls', {}).subscribe(
+      () => {
+        this.checkImage();
+      },
+      () => {
+        console.error('Error pulling image');
+      }
+    );
+  }
+
+  createSite() {
+    const body = this.siteForm.value
+
+    this.http.post('/sites', body).subscribe(
+      () => {
+        alert('Site created successfully!');
+        this.listSites();
+      },
+      () => {
+        console.error('Error creating site');
+      }
+    );
+  }
+
+  listSites() {
+    this.http.get<any[]>('/sites').subscribe(
+      sites => {
+        this.sites = sites;
+      },
+      () => {
+        console.error('Error fetching sites');
+      }
+    );
+  }
+
+  deleteSite(siteId: string) {
+    this.http.delete(`/sites/${siteId}`).subscribe(
+      () => {
+        alert('Site deleted successfully!');
+        this.listSites();
+      },
+      () => {
+        console.error('Error deleting site');
+      }
+    );
+  }
 }
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -6,9 +6,18 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NzLayoutModule} from "ng-zorro-antd/layout";
+import {NzBreadCrumbModule} from "ng-zorro-antd/breadcrumb";
+import {NzPageHeaderModule} from "ng-zorro-antd/page-header";
+import {NzSpinModule} from "ng-zorro-antd/spin";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzCheckboxModule} from "ng-zorro-antd/checkbox";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzTypographyModule} from "ng-zorro-antd/typography";
 
 registerLocaleData(en);
 
@@ -20,7 +29,17 @@ registerLocaleData(en);
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzPageHeaderModule,
+    NzSpinModule,
+    NzFormModule,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzInputModule,
+    ReactiveFormsModule,
+    NzTypographyModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
