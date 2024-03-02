52b5fb2e7462357c882ddb851b1bce0991c43409
naveen <kmnaveen101@gmail.com>
2024-03-02 12:19:51 +0530

    see #9: preserve containers

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from utils import DB
 from routers import sites, images
 
-
 app = FastAPI()
 origins = ["http://localhost:4200"]
 app.add_middleware(
@@ -21,28 +20,39 @@ app.include_router(sites.router)
 app.include_router(images.router)
 app.mount("/ui", StaticFiles(directory="ui/dist/ui/"))
 
+
 @app.get("/")
 async def root():
     return RedirectResponse(url="/ui/index.html")
 
+
 # Remove and prune containers on shutdown hook
 # Function to stop and remove containers with a specific prefix
-def stop_and_remove_containers(prefix: str):
+def stop_containers(prefix: str):
     client = docker.from_env()
     containers = client.containers.list(all=True, filters={"name": f"{prefix}*"})
     for container in containers:
         container.stop()
-        container.remove()
+
+
+# Fun
+def start_containers(prefix: str):
+    client = docker.from_env()
+    containers = client.containers.list(all=True, filters={"name": f"{prefix}*"})
+    for container in containers:
+        container.start()
+
 
 @app.on_event("shutdown")
-async def cleanup():
-    # Stop and remove containers with prefix "1clickwp"
-    stop_and_remove_containers("1clickwp")
+async def shutdown():
+    # only stop containers with prefix "1clickwp"
+    stop_containers("1clickwp")
+
 
 @app.on_event("startup")
-async def cleanup():
-    # Remove orphaned containers.
-    stop_and_remove_containers("1clickwp")
+async def startup():
+    # start containers if they are available.
+    start_containers("1clickwp")
     db = DB()
     db.init_db()
     db.init_phpmyadmin()
